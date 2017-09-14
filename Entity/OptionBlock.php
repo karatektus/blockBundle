254291d59c3218748b02391d1e07baf6ce93cfde
Paul Lützner <pl@pmdevelopment.de>
2017-09-14 12:44:13 +0200

    update

--- Entity/OptionBlock.php
+++ Entity/OptionBlock.php
@@ -3,7 +3,6 @@
 namespace Pluetzner\BlockBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 use Pluetzner\BlockBundle\Framework\Traits\EditableEntityTrait;
 
 /**
